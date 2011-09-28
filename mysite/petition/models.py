d446b7a0cace068be5f1f1eba35fde4c2809ba28
Magnus Blütecher Dysthe <magnusbd@stud.ntnu.no>
2011-09-28 23:08:23 +0200

    models.py

--- mysite/petition/models.py
+++ mysite/petition/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Kampanje(models.Model):
+    tittel = models.CharField("tittel", max_length=50)
+    beskrivelse = models.TextField("beskrivelse")
+    start_dato = models.DateTimeField("startdato", auto_now_add=True)
+    slutt_dato = models.DateTimeField("sluttdato", auto_now_add=True)
+
+class Underskrift(models.Model):
+    bruker_navn = models.CharField("navn", max_length=12)
+    kampanje = models.ForeignKey(Kampanje)
+
