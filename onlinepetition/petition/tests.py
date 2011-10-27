e8caa26eb5551c88d87a5023695e653195c26913
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2011-10-28 00:17:15 +0200

    unit test: test valid domains function.

--- onlinepetition/petition/tests.py
+++ onlinepetition/petition/tests.py
@@ -1,16 +1,32 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+OnlinePetition tests.
 
-Replace this with more appropriate tests for your application.
+Run with  manage.py test petition
 """
+from datetime import timedelta, datetime
 
 from django.test import TestCase
+from petition.models import Campaign, Signature, Domain
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PetitionTests(TestCase):
+
+    def setUp(self):
+        self.valid_domain = Domain.objects.create(name="uib.no")
+
+        self.campaign = Campaign.objects.create(
+            title="Test",
+            description="descriptionfoo",
+            start_date=datetime.now(),
+            end_date=datetime.now() + timedelta(hours=1),
+            )
+
+
+    def test_valid_domains_ends_with(self):
+        self.assertTrue(self.campaign.is_valid_domain('foobar@troll.no'))
+        self.campaign.valid_domains.add(self.valid_domain)
+        self.assertFalse(self.campaign.is_valid_domain('foobar@troll.no'))
+        self.assertTrue(self.campaign.is_valid_domain('student@stud.uib.no'))
+        self.assertTrue(self.campaign.is_valid_domain('admin@uib.no'))
+
+    
