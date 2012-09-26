f3945122d84dee4646db4748bc4b6f4428417f21
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2012-09-26 19:28:06 +0200

    Swapped email and name fields
    Should hopefully fix this following use case:
    User enter first email, usually enters his private password for his next step,
    this is scary as the next field is the full name and whoops
    the users password is shown in the list of signatures after validating it.
    
    Since we now take user's name first, it's less probability for the user
    to type in his password when jumping to the next field ;-)

--- onlinepetition/petition/forms.py
+++ onlinepetition/petition/forms.py
@@ -3,8 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 
 class CampaignRegistrationForm(forms.Form):
     id = "campaign_registration_form"
-    email = forms.EmailField("email", required=True, label=_("Email"))
     name = forms.CharField("name", required=True, label=_("Name"))
+    email = forms.EmailField("email", required=True, label=_("Email"))
 
     def clean(self):
         if self._errors:
